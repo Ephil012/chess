71e7e69fb17f6341ac74e262658e32a54e713a86
Ephil012 <Etp5501@gmail.com>
2022-08-23 14:59:58 -0400

    Fixed bugs with board not storing multiple states

--- src/chess.js
+++ src/chess.js
@@ -1,28 +1,48 @@
+const output_queens = (grid, n) =>  {
+    let results = []
+    for (let i = 0; i < n; i++) {
+        let row = ""
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j].includes('q')) {
+                row = row.concat("Q")
+            } else {
+                row = row.concat(".")
+            }
+        }
+        results.push(row)
+    }
+    return results
+}
+
 const n_queens = (n) => {
     let grid = []
     for (let i = 0; i < n; i++) {
         let row = []
         for (let j = 0; j < n; j++) {
-            row.push(null)
+            row.push([null])
         }
         grid.push(row)
     }
-    recurse(grid, 0, n)
+    let results = recurse(grid, 0, n)
+    console.log(results.length)
+    for (let i = 0; i < results.length; i++) {
+        console.log('[' + output_queens(results[i], n).toString() + ']')
+    }
 }
 
 const extendMarkers = (grid, column) => {
-    for(let i = 0; i < grid.length; i++) {
-        if (grid[i][column - 1] == "h") {
-            grid[i][column] = "h"
+    for (let i = 0; i < grid.length; i++) {
+        if (grid[i][column - 1].includes("h")) {
+            grid[i][column].push("h")
         }
         if (grid.length > i - 1 && i - 1 >= 0) {
-            if (grid[i - 1][column - 1] == "dn") {
-                grid[i][column] = "dn"
+            if (grid[i - 1][column - 1].includes("dn")) {
+                grid[i][column].push("dn")
             }
         }
         if (grid.length > i + 1 && i + 1 >= 0) {
-            if (grid[i + 1][column - 1] == "du") {
-                grid[i][column] = "du"
+            if (grid[i + 1][column - 1].includes("du")) {
+                grid[i][column].push("du")
             }
         }
     }
@@ -30,26 +50,22 @@ const extendMarkers = (grid, column) => {
 }
 
 const checkQueen = (grid, column, row) => {
-    if (grid[row][column] == "q" || grid[row][column] == "h" || grid[row][column] == "du" || grid[row][column] == "dn") {
-        return false
-    }
-    return true
+    return grid[row][column].includes("h") || grid[row][column].includes("du") || grid[row][column].includes("dn")
 }
 
 const addQueen = (grid, column, row) => {
-    grid = extendMarkers(grid, column)
-    if (checkQueen(grid, column, row) == false) {
+    if (checkQueen(grid, column, row)) {
         return null
     }
     let copied = JSON.parse(JSON.stringify(grid));
-    copied[row][column] = "q"
+    copied[row][column] = ["q"]
     if (copied[row].length > column + 1) {
-        copied[row][column + 1] = "h"
+        copied[row][column + 1] = ["h"]
         if (copied.length > row + 1 && row + 1 >= 0) {
-            copied[row + 1][column + 1] = "dn"
+            copied[row + 1][column + 1] = ["dn"]
         }
         if (copied.length > row - 1 && row - 1 >= 0) {
-            copied[row - 1][column + 1] = "du"
+            copied[row - 1][column + 1] = ["du"]
         }
     }
     return copied
@@ -60,20 +76,26 @@ const recurse = (grid, column, n) => {
     if (column == 0) {
         for (let i = 0; i < n; i++) {
             let copied = addQueen(grid, column, i)
-            copies.push(recurse(copied, column + 1, n))
+            let res = recurse(copied, column + 1, n)
+            if (res.length > 0) {
+                copies.push(...res)
+            }
         }
     } else if (column == n) {
-        console.table(grid)
-        return grid
+        return [grid]
     } else {
+        grid = extendMarkers(grid, column)
         for (let i = 0; i < n; i++) {
             let copied = addQueen(grid, column, i)
             if (copied != null) {
-                copies.push(recurse(copied, column + 1, n))
+                let res = recurse(copied, column + 1, n)
+                if (res.length > 0) {
+                    copies.push(...res)
+                }
             }
         }
     }
     return copies
 }
 
-n_queens(4)
+n_queens(9)
